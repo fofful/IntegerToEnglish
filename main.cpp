1f5f7a68f42a6676ee7212e2d33be450afec6f66
topi huttunen <topi.huttunen@tuni.fi>
2021-07-10 21:24:12 +0300

    should be working as expected

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-std::string returnTens(char number){
+std::string returnOnes(char number){
     switch(number){
         case '0':
-            return "zero";
+            return "";
         case '1':
             return "one";
         case '2':
@@ -25,27 +25,84 @@ std::string returnTens(char number){
     }
 }
 
+std::string returnTens(char number){
+    switch(number){
+        case '0':
+            return "";
+        case '1':
+            return "one";
+        case '2':
+            return "twenty";
+        case '3':
+            return "thirty";
+        case '4':
+            return "fourty";
+        case '5':
+            return "fifty";
+        case '6':
+            return "sixty";
+        case '7':
+            return "seventy";
+        case '8':
+            return "eightty";
+        case '9':
+            return "ninety";
+    }
+}
+
+std::string tenToTwenty(std::string numbersStr){
+    int numbersInt = std::stoi(numbersStr);
+    switch(numbersInt){
+        case 10:
+            return "ten";
+        case 11:
+            return "eleven";
+        case 12:
+            return "twelve";
+        case 13:
+            return "thirteen";
+        case 14:
+            return "fourteen";
+        case 15:
+            return "fifteen";
+        case 16:
+            return "sixteen";
+        case 17:
+            return "seventeen";
+        case 19:
+            return "eighteen";
+        case 20:
+            return "nineteen";
+    }
+}
+
 std::string returnHundreds(std::string numbers, int scale){
     std::string returnString = "";
     std::string numbersStr = numbers;
-    std::string multiplier [5] = {"thousand", "million", "billion", "hundred", "ten"};
+    std::string multiplier [4] = {"thousand", "million", "billion", "hundred"};
     int length = numbersStr.length();
     for (int i = length; i > 0; i--){
         if (i == 3){
-            returnString.append(returnTens(numbersStr.front()) + " ");
+            returnString.append(returnOnes(numbersStr.front()) + " ");
             returnString.append(multiplier[3] + " ");
             numbersStr = numbersStr.erase(0, 1);
         }
         else if (i == 2)
         {
+            if (numbersStr.front() == '1'){
+                returnString.append(tenToTwenty(numbersStr) + " ");
+                if (scale >= 1){
+                    returnString.append(multiplier[scale-1] + " ");
+                }
+                break;
+            }
             returnString.append(returnTens(numbersStr.front()) + " ");
-            returnString.append(multiplier[4] + " ");
             numbersStr = numbersStr.erase(0, 1);
         }
         else if (i == 1)
         {
-            returnString.append(returnTens(numbersStr.front()) + " ");
-            if (scale > 1){
+            returnString.append(returnOnes(numbersStr.front()) + " ");
+            if (scale >= 1){
                 returnString.append(multiplier[scale-1] + " ");
             }
             numbersStr = numbersStr.erase(0, 1);
