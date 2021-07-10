d8357ef40283ab9ed0f6018bcc057111c97d01c5
topi huttunen <topi.huttunen@tuni.fi>
2021-07-10 20:56:37 +0300

    Working up to without the twelves twenty etc. words

--- main.cpp
+++ main.cpp
@@ -1,31 +1,79 @@
 #include <iostream>
 
-std::string returnHundreds(std::string numbers){
-    std::cout << numbers << "dsadad "<<  std::endl;
-    return "asd";
+std::string returnTens(char number){
+    switch(number){
+        case '0':
+            return "zero";
+        case '1':
+            return "one";
+        case '2':
+            return "two";
+        case '3':
+            return "three";
+        case '4':
+            return "four";
+        case '5':
+            return "five";
+        case '6':
+            return "six";
+        case '7':
+            return "seven";
+        case '8':
+            return "eight";
+        case '9':
+            return "nine";
+    }
+}
+
+std::string returnHundreds(std::string numbers, int scale){
+    std::string returnString = "";
+    std::string numbersStr = numbers;
+    std::string multiplier [5] = {"thousand", "million", "billion", "hundred", "ten"};
+    int length = numbersStr.length();
+    for (int i = length; i > 0; i--){
+        if (i == 3){
+            returnString.append(returnTens(numbersStr.front()) + " ");
+            returnString.append(multiplier[3] + " ");
+            numbersStr = numbersStr.erase(0, 1);
+        }
+        else if (i == 2)
+        {
+            returnString.append(returnTens(numbersStr.front()) + " ");
+            returnString.append(multiplier[4] + " ");
+            numbersStr = numbersStr.erase(0, 1);
+        }
+        else if (i == 1)
+        {
+            returnString.append(returnTens(numbersStr.front()) + " ");
+            if (scale > 1){
+                returnString.append(multiplier[scale-1] + " ");
+            }
+            numbersStr = numbersStr.erase(0, 1);
+        }
+    }
+    return returnString;
 }
 
 int main(){
-    std::cout << "give an integer number: " << std::endl;
-    int num = 0;
-    std::string numAsStr;
-    std::string answer;
     
-    answer.empty();
-    numAsStr.empty();
+    int num = 0;
+    std::string numAsStr = "";
+    std::string answer = "";
+    std::string numbers = "";
 
+    std::cout << "give an integer number: " << std::endl;
     std::cin >> num;
     numAsStr = std::to_string(num);
     int scale = (numAsStr.length() - (numAsStr.length() % 3)) / 3;
-    std::cout << "scale is: " << scale << std::endl;
-    for (int i = scale; i > 0; i--){
-        std::cout << "numAsStr is: " << numAsStr << std::endl;
-        std::string numbers = numAsStr.substr(0, numAsStr.length()-((i-1)*3));
-        numbers = returnHundreds(numbers);
+
+    for (int i = scale; i >= 0; i--){
+        numbers.empty();
+        numbers = numAsStr.substr(0, numAsStr.length()-(i*3));
+        numbers = returnHundreds(numbers, i);
         answer.append(numbers);
-        numAsStr.erase(0,3);
+        numAsStr.erase(0, numAsStr.length()-(i*3));
     };
+
     std::cout << "number given in english is: " << answer << std::endl;
     return 0;
 }
-
